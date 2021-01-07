9586e2624f98ece48036e11e3e97c09a6fc6a2d5
Maxime Daniel <root@maxux.net>
2021-01-08 00:32:23 +0100

    init: specify absolute zdb path

--- init.sh
+++ init.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-zdb --datasize $((32 * 1024 * 1024)) --mode seq --listen 127.0.0.1 --hook /lib/zdb/zstor-hook.sh --background
+zdb --datasize $((32 * 1024 * 1024)) --mode seq --listen 127.0.0.1 --hook /lib/zdb/zstor-hook.sh --background --data /zdb/data --index /zdb/index
 nscreate
 zdbfs /mnt &
 
