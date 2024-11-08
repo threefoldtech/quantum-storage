bfe95e06ff7366cf7bb079f509f3b536aa1f7d4f
Scott Yeager <yeagersm@gmail.com>
2024-11-07 19:27:07 -0800

    Fix typo

--- pulumi/tests/rebuild/scripts/redeploy.sh
+++ pulumi/tests/rebuild/scripts/redeploy.sh
@@ -12,7 +12,7 @@ ipv6=$(pulumi stack -s test | grep pub_ipv6 | tr -s " " | cut -d ' ' -f 3 | cut 
 
 ssh -t root@$ipv6 '
   pkill zstor -SIGUSR1
-  # Wait some time to let the rebuild process start. This sould be enough?
+  # Wait some time to let the rebuild process start. This should be enough?
   sleep 10 
   # Output should show us if any data has been written to the new backends yet
   zstor -c /etc/zstor-default.toml status
