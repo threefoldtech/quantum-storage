c231e5f1e789158e3041f2d4cf8f80ee0e14032d
Scott Yeager <yeagersm@gmail.com>
2024-12-18 16:40:21 -0800

    Don't delete stack unless `down` succeeds

--- pulumi/tests/test-scripts-local/destroy.sh
+++ pulumi/tests/test-scripts-local/destroy.sh
@@ -3,5 +3,5 @@
 # We need this to run non interactively. Otherwise we'll be prompted for it
 export PULUMI_CONFIG_PASSPHRASE=""
 
-pulumi down -s test -y --non-interactive
-pulumi stack rm -yf test
+# If we fail to delete the deployment, we should keep the stack around
+pulumi down -s test -y --non-interactive && pulumi stack rm -yf test
