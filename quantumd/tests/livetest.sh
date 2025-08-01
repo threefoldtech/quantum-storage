faf80262d3d68aaa4eb2b9bf499451de81ffddb6
Scott Yeager <yeagersm@gmail.com>
2025-08-01 11:27:06 -0700

    Update livetest.sh

--- quantumd/tests/livetest.sh
+++ quantumd/tests/livetest.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 make docker-build
-docker rm -f quantumd-test && docker run -d --privileged --name quantumd-test quantumd:latest
+docker rm -f quantumd-test
+docker run -d --privileged --name quantumd-test quantumd:latest
 docker cp config.yaml quantumd-test:/etc/quantumd.yaml
 docker exec -it --env-file .env quantumd-test bash
