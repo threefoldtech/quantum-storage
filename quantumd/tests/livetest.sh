54ff00decb92f00e84645d5b8943a559446e1be5
Scott Yeager <yeagersm@gmail.com>
2025-08-15 12:22:15 -0700

    Set env file at run not exec

--- quantumd/tests/livetest.sh
+++ quantumd/tests/livetest.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 make docker-build
 docker rm -f quantumd-test
-docker run -d --privileged --name quantumd-test quantumd:latest
+docker run -d --privileged --env-file .env --name quantumd-test quantumd:latest
 docker cp config.yaml quantumd-test:/etc/quantumd.yaml
-docker exec -it --env-file .env quantumd-test bash
+docker exec -it  quantumd-test bash
